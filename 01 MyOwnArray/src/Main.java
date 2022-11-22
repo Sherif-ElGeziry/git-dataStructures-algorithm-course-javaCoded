8f5a6c8f7358d6dfa501d19127a11621d7d871aa
Sherif-ElGeziry <118230210+Sherif-ElGeziry@users.noreply.github.com>
2022-11-22 07:33:58 +0200

    Added get, push and replace methods

--- 01 MyOwnArray/src/Main.java
+++ 01 MyOwnArray/src/Main.java
@@ -2,8 +2,26 @@
 public class Main {
 
 	public static void main(String[] args) {
-		
+		MyOwnArray arr1 = new MyOwnArray();
+		System.out.println(arr1.get(0));
 
+		arr1.push("My");
+		arr1.push("name");
+		arr1.push("is");
+		arr1.push("Sherif");
+		// arr1.insert(1, "Test");
+		System.out.println(arr1.get(0));
+		System.out.println(arr1.get(1));
+		System.out.println(arr1.get(2));
+		System.out.println(arr1.get(3));
+//		System.out.println(arr1.get(4));
+
+		System.out.println();
+		arr1.replace(-1, "first name");
+		System.out.println(arr1.get(0));
+		System.out.println(arr1.get(1));
+		System.out.println(arr1.get(2));
+		System.out.println(arr1.get(3));
 	}
 
 }
--- 01 MyOwnArray/src/MyOwnArray.java
+++ 01 MyOwnArray/src/MyOwnArray.java
@@ -1,4 +1,68 @@
+//Implementation of our own Dynamic Array Data Structure
 
 public class MyOwnArray {
+	private String[] array; // we will store our elements in the array
+	private int capacity; // capacity is the number of elements that our array can hold
+	private int currentLength; // currentLength is the number of elements currently present in the array
 
+	public MyOwnArray() {
+		array = new String[1];
+		capacity = 1;
+		currentLength = 0;
+	}
+
+	// get(index) method returns the element of array at the given index
+	public String get(int index) {
+		if (index > -1 && index < currentLength) {
+			return this.array[index];
+		} else
+			return "Index out of bound";
+	}
+
+	// push() method adds an element at the end
+	public void push(String text) {
+		// if currentLength becomes equal to capacity we need to increase the capacity
+		// to add more elements
+		if (capacity == currentLength) {
+			String[] temp = new String[2 * capacity]; // create a new Array with double capacity
+
+			for (int i = 0; i < array.length; i++)
+				temp[i] = array[i]; // copy elements of old array to new array
+
+			array = temp; // set the newly created array as our real array
+			capacity *= 2; // update the value of capacity
+		}
+
+		array[currentLength] = text; // insert the new element at the end
+		currentLength++; // Increase value of currentLength by 1
+	}
+
+//	public void insert(int index, String text) {
+//		String[] temp = new String[array.length];
+//		for (int i = 0; i < array.length; i++) {
+//			if (i < index)
+//				temp[i] = array[i];
+//			else if (i == index)
+//				temp[i] = text;
+//			else
+//				temp[i] = array[i - 1];
+//		}
+//
+//		array = temp;
+//		currentLength++;
+//		capacity = currentLength * 2;
+////		array[index] = text;
+////		for (int i = index + 1; i < array.length; i++) {
+////			array[i] = temp[i];
+////		}
+//	}
+
+	// replace(index,value) replaces value at given index with the newly entered
+	// value
+	public void replace(int index, String text) {
+		if (index > -1 && index < currentLength) {
+			array[index] = text;
+		} else
+			System.out.println("Index out of bound");
+	}
 }
