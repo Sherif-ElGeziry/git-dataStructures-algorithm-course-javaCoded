104b63169e3e68020a576f482f8ee14a7b86beca
Sherif-ElGeziry <118230210+Sherif-ElGeziry@users.noreply.github.com>
2022-12-01 15:51:21 +0200

    Updated

--- 07 SinglyLinkedList/src/Main.java
+++ 07 SinglyLinkedList/src/Main.java
@@ -1,9 +1,35 @@
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		LinkedList linkedList = new LinkedList(10);
 
+		// append
+		linkedList.append(12);
+		linkedList.append(16);
+
+		// prepend
+		linkedList.prepend(20);
+		linkedList.prepend(50);
+		System.out.println("length: " + linkedList.getLength());
+		System.out.println("list: " + Arrays.toString(linkedList.printList()));
+
+		// insert
+		linkedList.insert(3, 25);
+		System.out.println("length: " + linkedList.getLength());
+		System.out.println("list: " + Arrays.toString(linkedList.printList()));
+
+//        //remove
+		linkedList.remove(4);
+		System.out.println("length: " + linkedList.getLength());
+		System.out.println("list: " + Arrays.toString(linkedList.printList()));
+
+//        //reverse
+//		LinkedList linkedList2 = linkedList.reverse(linkedList);
+//		System.out.println("reverse linkedList" + Arrays.toString(linkedList2.printList()));
+
+		linkedList.reverse();
+		System.out.println("list: " + Arrays.toString(linkedList.printList()));
 	}
-
 }
