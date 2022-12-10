9f781835e0d29b12871a4183f5b93880c90ff45b
Sherif-ElGeziry <118230210+Sherif-ElGeziry@users.noreply.github.com>
2022-12-10 04:24:47 +0200

    Setting up main class requirements

---
+++ 14 PriorityQueue/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+//		PriorityQueue queue = new PriorityQueue();
+//        queue.enqueue("piyush", 3);
+//        queue.enqueue("rohan", 1);
+//        queue.enqueue("jeet", 2);
+//        queue.enqueue("john", 5);
+//        queue.enqueue("Dave", 4);
+//        System.out.println(Arrays.toString(queue.printQueue()));
+//        System.out.println(queue.dequeue());
+//        System.out.println(Arrays.toString(queue.printQueue()));
+//        System.out.println(queue.front().value);
+//        System.out.println(queue.rear().value);
+//        System.out.println(queue.isEmpty());
+	}
+
+}
